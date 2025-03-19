e85934307dcafaf6b18cb587d6ef34826b1b4765
ts-xie <jasonx3023@gmail.com>
2025-03-18 18:31:29 -0700

    add show/hide for clear completed

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -8,10 +8,10 @@ pub fn Footer(
     todos: ReadSignal<Vec<Todo>>,
     filters: ReadSignal<FilterView>,
     on_filter_update: impl FnMut(FilterView) + 'static,
-    on_clear_completed: impl FnMut(MouseEvent) + 'static
+    on_clear_completed: impl Fn(MouseEvent) + 'static + Send + Sync
 ) -> impl IntoView {
     let on_filter_update = Arc::new(Mutex::new(on_filter_update));
-
+    let on_clear_completed = Arc::new(on_clear_completed);
     view! {
         <footer class="footer">
             <span class="todo-count">
@@ -38,9 +38,18 @@ pub fn Footer(
                     })
                     .collect_view()}
             </ul>
-            <button class="clear-completed" on:click=on_clear_completed>
-                "Clear completed"
-            </button>
+            <Show
+                when=move || { todos.get().iter().any(|todo| todo.done) }
+            >
+                {
+                    let on_clear_completed = on_clear_completed.clone();
+                    view! {
+                        <button class="clear-completed" on:click=move |ev| on_clear_completed(ev)>
+                            "Clear completed"
+                        </button>
+                    }
+                }
+            </Show>
         </footer>
     }
 }
